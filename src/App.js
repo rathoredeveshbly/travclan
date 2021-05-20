f2612c611f3719388ab6717132cce13d347d1dab
Devesh S Rathore <rathoredeveshbly@gmail.com>
2021-05-20 16:46:26 +0530

    get the data from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [post, setPost] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(5);
+
+useEffect(() => {
+  const fetchPost = async () =>{
+    setloading(true);
+    const res = await axios.get('https://intense-tor-76305.herokuapp.com/merchants');
+    setPost(res.data);
+    setloading(false);
+  }
+
+  fetchPost();
+}, []);
+console.log(post);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">Hello</div>
   );
 }
 
